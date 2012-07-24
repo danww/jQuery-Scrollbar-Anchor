707bdae587c58ed0df87b2bdf6cebc60bdb0e412
Dan Willis <dan.willis@gmail.com>
2012-07-25 11:54:31 +1200

    Finally got around to adding the tweaks from the code review:
    http://codereview.stackexchange.com/questions/8977/anchoring-an-element-to-the-browsers-vertical-scrollbar

--- jquery.scrollbaranchor.js
+++ jquery.scrollbaranchor.js
@@ -10,18 +10,28 @@
       barFollower = function (e) {
         var target = $(e.data.target),
           window_height = $(window).height(),
+          max_target_offset = window_height - target.height(),
           scroll_position = $(window).scrollTop(),
           body_height = $('body').height(),
-          offset = scroll_position + ((scroll_position * (window_height - (settings.scrollbar_button_height * 2))) / body_height) + (((window_height - (settings.scrollbar_button_height)) * (window_height - (settings.scrollbar_button_height))) / (2 * body_height)) + settings.scrollbar_button_height - element_offset;
+          // Ratio of full body height to full height of the scroll area which doesn't include buttons
+          scroll_scale_factor = body_height / (window_height - (settings.scrollbar_button_height * 2)),
+          offset = 0;
 
-        if (body_height < window_height) { // if there's no scrollbar.
-          offset =(window_height / 2) + (target.height() / 2);
-        } else if (offset < scroll_position) { // top bounds
-          offset = scroll_position;
-        } else if (offset > scroll_position + window_height - target.height()) { // bottom bounds
-          offset = scroll_position + window_height - target.height();
+        if (body_height <= window_height) { // If there's no scrollbar.
+          offset = max_target_offset / 2 - element_offset; // Position halfway down the window
+        } else {
+          offset = settings.scrollbar_button_height
+            + (scroll_position + (window_height / 2)) / scroll_scale_factor // mid-window position reduced to scroll area scale
+            - element_offset; // offset for chosen 'position' based on element height - see notes below
         }
-        target.css('top', offset);
+
+        /* Some catches for when element height might mean it would get positioned outside the window */
+        if (offset < 0) { // Top bounds
+          offset = 0;
+        } else if (offset > max_target_offset) { // Bottom bounds
+          offset = max_target_offset;
+        }
+        target.css('top', scroll_position + offset);
       };
 
     return this.each(function () {
